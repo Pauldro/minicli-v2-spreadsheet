9d9b3f45e802dbebd5f78666e6c60b39bf53c2ee
Paul Gomez <pauldro952@outlook.com>
2026-06-02 13:37:13 -0500

    FEAT: add symfony apcu cache

---
+++ src/Util/Caches/SymfonyApcuCache.php
@@ -0,0 +1,15 @@
+<?php namespace Pauldro\Minicli\v2\PhpSpreadsheet\Util\Caches;
+// Psr
+use Psr\SimpleCache\CacheInterface;
+// Symfony
+use Symfony\Component\Cache\Adapter\ApcuAdapter;
+use Symfony\Component\Cache\Psr16Cache;
+
+
+class SymfonyApcuCache extends AbstractCache {
+    protected static function cache() : CacheInterface 
+    {
+        $apcuAdapter = new ApcuAdapter();
+        return new Psr16Cache($apcuAdapter);
+    }
+}
